3452732668a94670b4fc4386ce5f43b1410566dc
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-11-14 17:36:09 +0900

    feat: home & dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,17 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions',
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # トップページ
+  root to: 'home#index'
+
+  # ダッシュボード
+  get "dashboard" => "dashboard#index", as: :dashboard
+
+  # ヘルスチェック
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
+  # PWA関連
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root to: 'home#index'
 end

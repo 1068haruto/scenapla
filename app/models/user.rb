eb761b11fe67cb3a3c52eda898c6d68d68d3fca9
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-11-15 15:40:56 +0900

    feat: email authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   validates :password, presence: true, format: { with: /(?=.*[a-z])(?=.*\d)/, message: "は小文字と数字を含める必要があります" }
 end

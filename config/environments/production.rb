01cf3b25fbcacd4e231c11596ff204438b0b6b7c
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-11-19 01:43:48 +0900

    fix: edit mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,18 @@ Rails.application.configure do
   # caching is enabled.
   #config.action_mailer.perform_caching = false
 
+  #config.action_mailer.smtp_settings = {
+    #:port           => ENV['MAILGUN_SMTP_PORT'],
+    #:address        => ENV['MAILGUN_SMTP_SERVER'],
+    #:user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    #:password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    #:domain         => ENV['MAILGUN_DOMAIN'],
+    #:authentication => :plain,
+  #}
+
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'quiet-retreat-43609-63e9cfd4f8f2.herokuapp.com', protocol: 'https' }
+  host = 'https://quiet-retreat-43609-63e9cfd4f8f2.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],

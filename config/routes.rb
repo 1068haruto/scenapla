530deb875cca4164f72739b896d189caa47e4ca3
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-11-20 01:10:14 +0900

    feat: account page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions',
   }
+  resources :users, only: [:show]
 
   # トップページ
   root to: 'home#index'

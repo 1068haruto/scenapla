75b2ae05867b2e6f2bc7c77ed01a273377a61377
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-11-21 15:59:31 +0900

    feat: fix mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   #}
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: www.scenapla.com }
+  config.action_mailer.default_url_options = { host: 'www.scenapla.com' }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],

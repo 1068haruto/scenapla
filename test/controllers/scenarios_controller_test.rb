7aa1d9f7048c947efac3e4553f535db63d6c4cab
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-11-25 18:42:17 +0900

    feat: add scenario controller & view

---
+++ test/controllers/scenarios_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class ScenariosControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get scenarios_index_url
+    assert_response :success
+  end
+end

bbbabd6dc51ee46aa6a29fd2a07a77d6a76d2e86
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-11-27 02:09:52 +0900

    feat: add expense relationsn & create method & hotwire view

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,6 +1,7 @@
 class Simulation < ApplicationRecord
   belongs_to :user
   has_many :incomes
+  has_many :expenses
 
   validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :incomes, dependent: :destroy
+  has_many :expenses, dependent: :destroy
   has_one :simulation, dependent: :destroy
   has_many :scenarios, dependent: :destroy
   

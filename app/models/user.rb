29ec0c29212f969e1801a427e14fabcb41c140ef
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-11-28 19:27:22 +0900

    feat: add user_asset model & migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :incomes, dependent: :destroy
   has_many :expenses, dependent: :destroy
+  has_many :user_assets, dependent: :destroy
   has_one :simulation, dependent: :destroy
   has_many :scenarios, dependent: :destroy
   

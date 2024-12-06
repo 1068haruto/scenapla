ab6f6d644bf4e0b2209237a473517498e98de2ab
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-12-06 18:20:18 +0900

    feat: edit life_event form layout & remove hotwire

--- app/controllers/life_events_controller.rb
+++ app/controllers/life_events_controller.rb
@@ -14,7 +14,7 @@ class LifeEventsController < ApplicationController
   def create
     @life_event = LifeEvent.build(convert_event_date(life_event_params))
     if @life_event.save
-      respond_to_format
+      redirect_to new_life_event_path, notice: 'ライフイベント情報が追加されました！'
     else
       @life_events = LifeEvent.where(user_id: current_user.id).order(event_date: :asc)
       render :new
@@ -25,7 +25,7 @@ class LifeEventsController < ApplicationController
     @life_event = LifeEvent.find(params[:id])
 
     if @life_event.destroy
-      respond_to_format_destroy
+      redirect_to new_life_event_path, notice: 'ライフイベントが削除されました！'
     else
       redirect_to new_life_event_path, alert: 'ライフイベントの削除に失敗しました。'
     end
@@ -35,9 +35,9 @@ class LifeEventsController < ApplicationController
     simulation_id = params[:simulation_id]
   
     if LifeEvent.update_simulation_data(simulation_id)
-      redirect_to new_life_event_path, notice: 'LifeEventDataが更新されました。'
+      redirect_to new_life_event_path, notice: 'シミュレーション情報が更新されました。'
     else
-      redirect_to new_life_event_path, alert: 'LifeEventDataの更新に失敗しました。'
+      redirect_to new_life_event_path, alert: 'シミュレーション情報の更新に失敗しました。'
     end
   end
 
@@ -55,21 +55,6 @@ class LifeEventsController < ApplicationController
       params[:event_date] = Date.new(params[:event_date].to_i, 1, 1)
     end
     params
-  end
-
-  def respond_to_format
-    respond_to do |format|
-      format.html { redirect_to life_events_path, notice: 'ライフイベントが保存されました！' }
-      format.turbo_stream
-    end
-  end
-
-  def respond_to_format_destroy
-    respond_to do |format|
-      format.html { redirect_to life_events_path, notice: 'ライフイベントが削除されました！' }
-      format.turbo_stream { render turbo_stream: turbo_stream.remove(@life_event) }
-      #format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@user_asset)) }
-    end
   end
 
   # 年代を計算するメソッド

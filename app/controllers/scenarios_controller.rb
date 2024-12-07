3969495bfa3d04c4fd7a735df53857f7d3ec8e58
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-12-08 05:36:02 +0900

    feat: add asset_data view

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -2,12 +2,11 @@ class ScenariosController < ApplicationController
   def index
     @simulation = Simulation.find_by(user_id: current_user.id)
     @scenario = Scenario.find_by(user_id: current_user.id)
-    #現実的シナリオ
-    @chart_data = @scenario.balance_chart_data
-    #@total_income = @scenario.total_income
-    #@total_expense = @scenario.total_expense
-    #@total_balance = @total_expense + @total_income
 
+    #資産シナリオ
+    @asset_data = @scenario.simulation.user_asset_data.map { |entry| [entry["date"], entry["amount"]] }.to_h
+    #現実的シナリオ
+    @real_balance_chart_data = @scenario.balance_chart_data
     @real_total_income = @scenario.total_income || 0
     @real_total_expense = @scenario.total_expense || 0
     @real_total_balance = @scenario.total_balance || 0

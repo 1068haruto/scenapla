f69bd91c507c6216b3cf8eb8114cc4f9d1f0089a
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-12-14 03:36:18 +0900

    fix: add dependent destroy to associations in Simulation

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,9 +1,9 @@
 class Simulation < ApplicationRecord
   belongs_to :user
-  has_many :incomes
-  has_many :expenses
-  has_many :user_assets
-  has_many :life_events
+  has_many :incomes, dependent: :destroy
+  has_many :expenses, dependent: :destroy
+  has_many :user_assets, dependent: :destroy
+  has_many :life_events, dependent: :destroy
   has_many :scenarios, dependent: :destroy
   has_many :asset_lifespans, dependent: :destroy
 

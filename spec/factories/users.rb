5e8b621add99949bfbd98866a83e3dff525f82fd
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2025-01-26 22:00:36 +0900

    feat:  User model tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :user do
     name { "テストユーザー" }
     email { "test@example.com" }
-    password { "password" }
+    date_of_birth { "1990-01-01" }
+    password { "password1234" }
+    password_confirmation { password }
   end
 end

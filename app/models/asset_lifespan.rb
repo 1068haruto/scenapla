e5dffc1a90207c9525ca145d29f30d85a20be351
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2025-02-02 07:51:53 +0900

    refactor: simulation model

--- app/models/asset_lifespan.rb
+++ app/models/asset_lifespan.rb
@@ -3,4 +3,15 @@ class AssetLifespan < ApplicationRecord
   belongs_to :simulation
 
   validates :yearly_lifespans, presence: true
+
+  # 資産寿命データの更新
+  def self.update_lifespan_data!(simulation, yearly_lifespan, lifespan_years, lifespan_months)
+    lifespan = simulation.asset_lifespans.find_or_initialize_by(user_id: simulation.user_id)
+    lifespan.update!(
+      user_id: simulation.user_id,
+      yearly_lifespans: yearly_lifespan,
+      lifespan_years: lifespan_years,
+      lifespan_months: lifespan_months
+    )
+  end
 end

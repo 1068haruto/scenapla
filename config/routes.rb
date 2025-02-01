e2a56953cef80344b60130ba289b0e4d0427e362
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2025-02-02 05:33:10 +0900

    refactor: simulation model

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,15 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
   devise_scope :user do
     # SNSログインユーザーの生年月日登録
     get "users/date_of_birth/edit", to: "users/registrations#edit_date_of_birth", as: "edit_date_of_birth"
     patch "users/date_of_birth", to: "users/registrations#update_date_of_birth", as: "update_date_of_birth"
   end
+
   resources :users, only: [ :show ]
+  # -----------------------------------------------------------
   resources :incomes do
     collection do
       post :update_simulation_data
@@ -36,12 +39,11 @@ Rails.application.routes.draw do
     end
   end
   resources :memos, only: %i[create update]
-  resources :simulations do
-    member do
-      post :update_scenario
+  resources :scenarios do
+    collection do
+      post :update_scenarios
     end
   end
-  resources :scenarios, only: [ :index ]
 
   # ニュースページ
   resources :news, only: [ :index ]

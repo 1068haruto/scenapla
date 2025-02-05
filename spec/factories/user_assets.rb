27849f474077c089cfcbbffdc4b708be8b6e37b7
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2025-02-06 03:56:54 +0900

    feat: user_assets request spec

--- spec/factories/user_assets.rb
+++ spec/factories/user_assets.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user_asset do
     user
     simulation
-    person_type { 0 }
-    asset_type { 2 }
+    person_type { "本人" }
+    asset_type { "預金" }
     amount { 10 }
     return_rate { 10 }
   end
---
+++ spec/requests/user_assets_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+RSpec.describe "UserAssets", type: :request do
+  let!(:user) { create(:user) }
+  let!(:simulation) { create(:simulation, user: user) }
+  let!(:user_asset) { create(:user_asset, user: user, simulation: simulation) }
+
+  let(:valid_params) { attributes_for(:user_asset) }
+  let(:invalid_params) { { amount: nil } }
+
+  before do
+    sign_in user
+  end
+
+  describe "GET /index" do
+    it "ページを表示し200を返す" do
+      get user_assets_path
+      expect(response).to have_http_status(:success) # 200
+    end
+  end
+
+  describe "POST /create" do
+    context "正しいフォーム入力で保存成功した場合" do
+      it "user_assets_pathへリダイレクトし302を返す" do
+        post user_assets_path, params: { user_asset: valid_params }
+        expect(response).to have_http_status(:found) # 302
+      end
+    end
+
+    context "不正フォーム入力で保存失敗した場合" do
+      it "422を返す" do
+        post user_assets_path, params: { user_asset: invalid_params }
+        expect(response).to have_http_status(:unprocessable_entity) # 422
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    context "既存データがあり、削除が成功した場合" do
+      it "user_assets_pathへリダイレクトし302を返す" do
+        delete user_asset_path(user_asset)
+        expect(response).to have_http_status(:found) # 302
+      end
+    end
+
+    context "既存データがなく、削除ができない場合" do
+      it "404を返す" do
+        delete user_asset_path(-1) # 存在しないIDを指定
+        expect(response).to have_http_status(:not_found) # 404
+      end
+    end
+  end
+
+  describe "POST /update_simulation_data" do
+    context "成功した場合" do
+      before do
+        allow_any_instance_of(Simulation).to receive(:update_user_asset_data!).and_return(true)
+      end
+
+      it "new_life_event_pathへリダイレクトし302を返す" do
+        post update_simulation_data_user_assets_path
+        expect(response).to have_http_status(:found) # 302
+      end
+    end
+
+    context "失敗した場合" do
+      before do
+        allow_any_instance_of(Simulation).to receive(:update_user_asset_data!).and_return(false)
+      end
+
+      it "user_assets_pathへリダイレクトし302を返す" do
+        post update_simulation_data_user_assets_path
+        expect(response).to have_http_status(:found) # 302
+      end
+    end
+  end
+end

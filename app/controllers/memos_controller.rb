36cf35a6a3406e915cbdf62ea41c75febde11d9a
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2025-02-08 03:37:14 +0900

    refactor: memos controller & view

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,14 +1,8 @@
 class MemosController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_memo, only: %i[update]
 
   def create
-    @memo = Memo.new(memo_params.merge(user_id: current_user.id))
-
-    Rails.logger.info "Received memo params: #{memo_params.inspect}"
-    Rails.logger.info "Memo object before save: #{@memo.inspect}"
-    puts params.inspect
-
+    @memo = current_user.memos.new(memo_params)
     if @memo.save
       render json: { message: "保存しました。", memo: @memo }
     else
@@ -17,18 +11,15 @@ class MemosController < ApplicationController
   end
 
   def update
+    @memo = current_user.memos.find_by(id: params[:id])
     if @memo.update(memo_params)
-      render json: { message: "更新しました。", memo: @memo }
+      render json: { message: "保存しました。", memo: @memo }
     else
-      render json: { message: "更新に失敗しました。", errors: @memo.errors.full_messages }, status: :unprocessable_entity
+      render json: { message: "保存に失敗しました。", errors: @memo.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
-
-  def set_memo
-    @memo = Memo.find_by(id: params[:id], user_id: current_user.id)
-  end
 
   def memo_params
     params.require(:memo).permit(:age_group, :content)

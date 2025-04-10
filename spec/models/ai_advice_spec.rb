33ab3654e752ecacd3c592ecaf9ce3bfc2bfa5ed
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2025-04-10 23:19:30 +0900

    feat: model spec for ai_advice

--- spec/models/ai_advice_spec.rb
+++ spec/models/ai_advice_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe AiAdvice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let(:ai_advice) { create(:ai_advice, user: user) }
+
+  describe 'アソシエーションテスト' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'バリデーションテスト' do
+    context '必須項目の確認' do
+      it 'user_idは必須' do
+        ai_advice.user_id = nil
+        expect(ai_advice).not_to be_valid
+      end
+
+      it 'contentは必須' do
+        ai_advice.content = nil
+        expect(ai_advice).not_to be_valid
+      end
+
+      it 'real_scenario_updated_atは必須' do
+        ai_advice.real_scenario_updated_at = nil
+        expect(ai_advice).not_to be_valid
+      end
+    end
+  end
 end

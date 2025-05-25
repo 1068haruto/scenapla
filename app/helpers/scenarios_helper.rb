0f1f113c1ebdfe3dc8e4f3537226eb9dbba4361d
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-05-25 14:45:40 +0900

    refactor: extract conditional logic to helper, improve text, rename partial

--- app/helpers/scenarios_helper.rb
+++ app/helpers/scenarios_helper.rb
@@ -1,14 +1,32 @@
 module ScenariosHelper
-  # 日付表示部分
+  # 条件分岐 -----------------------------
+  def show_asset_lifespan_scenario?
+    @asset_lifespan.present? && @asset_lifespan.asset_lifespan_scenario.present?
+  end
+
+  def show_balance_scenario?(balance_chart_data)
+    !balance_chart_data.empty? && @incomes.any? && @expenses.any?
+  end
+
+  def show_asset_scenario?
+    !@asset_data.empty? && @user_assets.any?
+  end
+
+  def show_next_button?(simulation)
+    simulation.user_asset_data.present? && simulation.user_asset_data.any?
+  end
+
+  # 日付表示部分 --------------------------
   def formatted_date(date)
     date.present? ? date.strftime("%Y年%m月%d日") : ""
   end
 
+  # チャート ------------------------------
   def lifespan_chart(data)
     column_chart(
       data,
       xtitle: "年",
-      ytitle: "資産額（年始時点）",
+      ytitle: "資産額",
       thousands: ",",
       suffix: "万円",
       download: { filename: "資産寿命シナリオ" }

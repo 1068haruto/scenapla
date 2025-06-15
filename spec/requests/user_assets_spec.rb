512343c65a992510fdc3d9f456d0c4d51ff2cc42
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-06-15 14:07:24 +0900

    feat: edit_form for user_asset

--- spec/requests/user_assets_spec.rb
+++ spec/requests/user_assets_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe "UserAssets", type: :request do
   let!(:simulation) { create(:simulation, user: user) }
   let!(:user_asset) { create(:user_asset, user: user, simulation: simulation) }
 
+  # パラメータ
   let(:valid_params) { attributes_for(:user_asset) }
-  let(:invalid_params) { { amount: nil } }
+  let(:valid_params_for_update) { { amount: 20 } }
+  let(:invalid_params) { { amount: -20 } }
 
   before do
     sign_in user
@@ -20,32 +22,67 @@ RSpec.describe "UserAssets", type: :request do
   end
 
   describe "POST /create" do
-    context "正しいフォーム入力で保存成功した場合" do
-      it "user_assets_pathへリダイレクトし302を返す" do
+    context "有効なパラメータの場合" do
+      it "作成成功し、302を返す" do
         post user_assets_path, params: { user_asset: valid_params }
         expect(response).to have_http_status(:found) # 302
       end
     end
 
-    context "不正フォーム入力で保存失敗した場合" do
-      it "422を返す" do
+    context "無効なパラメータの場合" do
+      it "作成失敗し、422を返す" do
         post user_assets_path, params: { user_asset: invalid_params }
         expect(response).to have_http_status(:unprocessable_entity) # 422
       end
     end
   end
 
+  describe "GET /edit" do
+    context "編集したいデータが存在する場合" do
+      it "編集フォームを表示し、200を返す" do
+        get edit_user_asset_path(user_asset)
+        expect(response).to have_http_status(:success) # 200
+      end
+    end
+
+    context "編集したいデータが存在しない場合" do
+      it "編集フォームを表示せず、404を返す" do
+        get edit_user_asset_path(-1) # 存在しないID
+        expect(response).to have_http_status(:not_found) # 404
+      end
+    end
+  end
+
+  describe "PATCH /update" do
+    context "有効なパラメータの場合" do
+      it "更新成功し、302を返す" do
+        patch user_asset_path(user_asset), params: { user_asset: valid_params_for_update }
+        expect(response).to have_http_status(:found) # 302
+
+        user_asset.reload
+        expect(user_asset.amount).to eq 20
+      end
+    end
+
+    context "無効なパラメータの場合" do
+      it "更新失敗し、422を返す" do
+        patch user_asset_path(user_asset), params: { user_asset: invalid_params }
+        expect(response).to have_http_status(:unprocessable_entity) # 422
+      end
+    end
+  end
+
   describe "DELETE /destroy" do
-    context "既存データがあり、削除が成功した場合" do
-      it "user_assets_pathへリダイレクトし302を返す" do
+    context "削除したいデータが存在する場合" do
+      it "削除成功し、302を返す" do
         delete user_asset_path(user_asset)
         expect(response).to have_http_status(:found) # 302
       end
     end
 
-    context "既存データがなく、削除ができない場合" do
-      it "404を返す" do
-        delete user_asset_path(-1) # 存在しないIDを指定
+    context "削除したいデータが存在しない場合" do
+      it "削除失敗し、404を返す" do
+        delete user_asset_path(-1) # 存在しないID
         expect(response).to have_http_status(:not_found) # 404
       end
     end

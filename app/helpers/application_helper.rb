2b5ea4c0d88f4d84b64f4ef46b60b1db28672159
1068haruto <harutokasuga.610@gmail.com>
2025-11-01 02:17:08 +0900

    refactor: Extract next & back button to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def facebook_app_id
     ENV["FACEBOOK_APP_ID"] || Rails.application.credentials.dig(:facebook, :app_id)
   end
+
+  def next_button_post(text, path)
+    button_to text, path, class: 'btn btn-primary', method: :post, data: { turbo: true }
+  end
+
+  def next_button_get(text, path)
+    link_to text, path, class: 'btn btn-primary', data: { turbo: true }
+  end
+
+  def back_button(text, path)
+    link_to text, path, class: 'btn btn-outline-secondary me-2', data: { turbo: true }
+  end
 end

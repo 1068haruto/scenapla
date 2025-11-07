9d20cb70d33bdf579ff5b1276b07de3a7f340f1b
1068haruto <harutokasuga.610@gmail.com>
2025-11-08 03:06:29 +0900

    refactor: request tests

--- spec/requests/incomes_spec.rb
+++ spec/requests/incomes_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Incomes", type: :request do
   end
 
   describe "GET /index" do
-    it "ページを表示し200を返す" do
+    it "ページを表示(200)" do
       get incomes_path
       expect(response).to have_http_status(:success)
     end
@@ -23,14 +23,14 @@ RSpec.describe "Incomes", type: :request do
 
   describe "POST /create" do
     context "有効なパラメータの場合" do
-      it "作成成功し、302を返す" do
+      it "作成成功(302)" do
         post incomes_path, params: { income: valid_params }
         expect(response).to have_http_status(:found)
       end
     end
 
     context "無効なパラメータの場合" do
-      it "作成失敗し、422返す" do
+      it "作成失敗(422)" do
         post incomes_path, params: { income: invalid_params }
         expect(response).to have_http_status(:unprocessable_entity)
       end
@@ -38,26 +38,26 @@ RSpec.describe "Incomes", type: :request do
   end
 
   describe "GET /edit" do
-    context "編集したいデータが存在する場合" do
-      it "編集フォームを表示し、200を返す" do
+    context "データが存在する場合" do
+      it "フォームを表示(200)" do
         get edit_income_path(income)
-        expect(response).to have_http_status(:success) # 200
+        expect(response).to have_http_status(:success)
       end
     end
 
-    context "編集したいデータが存在しない場合" do
-      it "編集フォームを表示せず、302を返す" do
+    context "データが存在しない" do
+      it "フォームを表示しない(302)" do
         get edit_income_path(-1) # 存在しないID
-        expect(response).to have_http_status(:found) # 302
+        expect(response).to have_http_status(:found)
       end
     end
   end
 
   describe "PATCH /update" do
     context "有効なパラメータの場合" do
-      it "更新成功し、302を返す" do
+      it "更新成功(302)" do
         patch income_path(income), params: { income: valid_params_for_update }
-        expect(response).to have_http_status(:found) # 302
+        expect(response).to have_http_status(:found)
 
         income.reload
         expect(income.monthly_income).to eq 20
@@ -65,25 +65,25 @@ RSpec.describe "Incomes", type: :request do
     end
 
     context "無効なパラメータの場合" do
-      it "更新失敗し、422を返す" do
+      it "更新失敗(422)" do
         patch income_path(income), params: { income: invalid_params }
-        expect(response).to have_http_status(:unprocessable_entity) # 422
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
   describe "DELETE /destroy" do
-    context "削除したいデータが存在する場合" do
-      it "削除成功し、302を返す" do
+    context "データが存在する場合" do
+      it "削除成功(302)" do
         delete income_path(income)
         expect(response).to have_http_status(:found)
       end
     end
 
-    context "削除したいデータが存在しない場合" do
-      it "削除失敗し、302を返す" do
+    context "データが存在しない場合" do
+      it "削除失敗(302)" do
         delete income_path(-1) # 存在しないID
-        expect(response).to have_http_status(:found) # 302
+        expect(response).to have_http_status(:found)
       end
     end
   end

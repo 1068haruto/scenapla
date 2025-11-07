1c6a808479598d8f0d201bb2a9b6c09b2efe94b9
1068haruto <harutokasuga.610@gmail.com>
2025-11-08 03:01:18 +0900

    refactor: tests

--- spec/models/income_spec.rb
+++ spec/models/income_spec.rb
@@ -1,102 +1,110 @@
 require 'rails_helper'
 
 RSpec.describe Income, type: :model do
-  describe 'アソシエーションテスト' do
+  describe 'relation' do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:simulation) }
   end
 
-  describe '定数テスト' do
-    it 'MONTHS_IN_A_YEARは12である' do
+  describe 'constant' do
+    it 'MONTHS_IN_A_YEARは「12」' do
       expect(described_class::MONTHS_IN_A_YEAR).to eq(12)
+    end
+
+    it 'JANUARYは「1」' do
+      expect(described_class::JANUARY).to eq(1)
+    end
+
+    it 'FIRSTは「1」' do
+      expect(described_class::FIRST).to eq(1)
     end
   end
 
-  describe 'enumテスト' do
+  describe 'enum' do
     it 'person_typesが正しい値を持つ' do
       expect(described_class.person_types).to eq({ "本人" => 0, "配偶者" => 1 })
     end
   end
 
-  describe 'バリデーションテスト' do
+  describe 'validation' do
     let(:income) { build(:income) }
 
-    context '必須項目の確認' do
-      it 'user_idは必須' do
+    context '必須項目' do
+      it 'user_idなしは、無効' do
         income.user_id = nil
         expect(income).not_to be_valid
       end
 
-      it 'simulation_idは必須' do
+      it 'simulation_idなしは、無効' do
         income.simulation_id = nil
         expect(income).not_to be_valid
       end
 
-      it '対象は必須' do
+      it '対象者なしは、無効' do
         income.person_type = nil
         expect(income).not_to be_valid
         expect(income.errors[:person_type]).to include("対象者を入力してください。")
       end
 
-      it '月収は必須' do
+      it '月収なしは、無効' do
         income.monthly_income = nil
         expect(income).not_to be_valid
         expect(income.errors[:monthly_income]).to include("月収を入力してください。", "数値を入力してください。")
       end
 
-      it '賞与年額は必須' do
+      it '賞与年額なしは、無効' do
         income.yearly_bonus = nil
         expect(income).not_to be_valid
         expect(income.errors[:yearly_bonus]).to include("賞与年額を入力してください。", "数値を入力してください。")
       end
 
-      it '退職時期は必須' do
+      it '退職時期なしは、無効' do
         income.retirement_date = nil
         expect(income).not_to be_valid
         expect(income.errors[:retirement_date]).to include("退職時期を入力してください。")
       end
 
-      it '退職金は必須' do
+      it '退職金なしは、無効' do
         income.retirement_pay = nil
         expect(income).not_to be_valid
         expect(income.errors[:retirement_pay]).to include("退職金を入力してください。", "数値を入力してください。")
       end
     end
 
-    context 'プラス値入力の確認' do
-      it '収入が0以上' do
+    context 'プラス値項目' do
+      it '収入がマイナスなら無効' do
         income.monthly_income = -1
         expect(income).not_to be_valid
         expect(income.errors[:monthly_income]).to include("月収はプラス値で入力してください。")
       end
 
-      it '収入が0以上' do
+      it '賞与がマイナスなら無効' do
         income.yearly_bonus = -1
         expect(income).not_to be_valid
         expect(income.errors[:yearly_bonus]).to include("賞与年額はプラス値で入力してください。")
       end
 
-      it '退職金が0以上' do
+      it '退職金がマイナスなら無効' do
         income.retirement_pay = -1
         expect(income).not_to be_valid
         expect(income.errors[:retirement_pay]).to include("退職金はプラス値で入力してください。")
       end
     end
 
-    context '数値入力の確認' do
-      it "月収が文字列の場合、無効" do
+    context '数値項目' do
+      it "月収がInt以外なら無効" do
         income.monthly_income = "abc"
         expect(income).not_to be_valid
         expect(income.errors[:monthly_income]).to include("数値を入力してください。")
       end
 
-      it "賞与が文字列の場合、無効" do
+      it "賞与年額がInt以外なら無効" do
         income.yearly_bonus = "abc"
         expect(income).not_to be_valid
         expect(income.errors[:yearly_bonus]).to include("数値を入力してください。")
       end
 
-      it "退職金が文字列の場合、無効" do
+      it "退職金がInt以外なら無効" do
         income.retirement_pay = "abc"
         expect(income).not_to be_valid
         expect(income.errors[:retirement_pay]).to include("数値を入力してください。")
@@ -104,74 +112,68 @@ RSpec.describe Income, type: :model do
     end
   end
 
-  describe 'カスタムセッターテスト' do
-    describe '#retirement_date=' do
-      it '入力された退職年を年始の日付に変換する' do
-        income = build(:income, retirement_date: "2030")
-        expect(income.retirement_date).to eq(Date.new(2030, 1, 1))
-      end
-    end
-  end
+  describe 'メソッド' do
+    let(:user) { create(:user) }
+    let(:simulation) { create(:simulation, user: user) }
+    year = Date.current.year
 
-  describe 'クラスメソッドテスト' do
     describe '.generate_income_data_for' do
-      let(:user) { create(:user) }
-      let(:simulation) { create(:simulation, user: user) }
-
-      it '指定されたユーザーの全収入データを1つのハッシュ配列に整形すること' do
-        create(:income, user: user, simulation: simulation, monthly_income: 1, yearly_bonus: 1, retirement_date: 2030, retirement_pay: 1)
-        create(:income, user: user, simulation: simulation, monthly_income: 1, yearly_bonus: 1, retirement_date: 2030, retirement_pay: 1)
-
+      it '全収入データを1つのオブジェクト配列にする' do
+        create(:income, user: user, simulation: simulation)
+        create(:income, user: user, simulation: simulation)
         result = Income.generate_income_data_for(user)
 
         expect(result).to include(
-          { amount: 26, date: 2025 }, { amount: 26, date: 2026 }, { amount: 26, date: 2027 },
-          { amount: 26, date: 2028 }, { amount: 26, date: 2029 }, { amount: 28, date: 2030 }
+          { amount: 26, date: year },
+          { amount: 26, date: year + 1 },
+          { amount: 26, date: year + 2 },
+          { amount: 26, date: year + 3 },
+          { amount: 26, date: year + 4 },
+          { amount: 28, date: year + 5 }
         )
-        expect(result).to include({ date: Date.current.year, amount: 26 })
-        expect(result).to include({ date: 2030, amount: 28 })
+        expect(result).to include({ date: year, amount: 26 })
+        expect(result).to include({ date: year + 5, amount: 28 })
+        expect(result.length).to eq(6)
       end
     end
 
-    describe '.format_grouped_data' do
-      it '# 年毎に収入を合計して整形すること' do
-        grouped_data = {
-          2025 => [ { date: 2025, amount: 5 } ], 2030 => [ { date: 2030, amount: 1 }, { date: 2030, amount: 2 } ]
-        }
-
-        result = Income.send(:format_grouped_data, grouped_data)
-
-        expect(result).to include({ date: 2025, amount: 5 }, { date: 2030, amount: 3 })
-      end
-    end
-  end
-
-  describe 'インスタンスメソッドテスト' do
-    describe '#calculate_income_until_retirement' do
-      it '現在〜退職まで年毎に年収のハッシュ配列を作成する' do
-        income = build(:income, retirement_date: 2030)
-        result = income.calculate_income_until_retirement
+    describe '#calculate_until_retirement' do
+      it '現在〜退職までの年収を計算し、オブジェクト配列とする' do
+        income = build(:income)
+        result = income.calculate_until_retirement
 
         expect(result).to include(
-          { amount: 12, date: 2025 }, { amount: 12, date: 2026 }, { amount: 12, date: 2027 },
-          { amount: 12, date: 2028 }, { amount: 12, date: 2029 }, { amount: 13, date: 2030 }
+          { amount: 13, date: year },
+          { amount: 13, date: year + 1 },
+          { amount: 13, date: year + 2 },
+          { amount: 13, date: year + 3 },
+          { amount: 13, date: year + 4 },
+          { amount: 14, date: year + 5 }
         )
-        expect(result).to include({ date: Date.current.year, amount: 12 })
-        expect(result.length).to eq(2030 - Date.current.year + 1)
+        expect(result.length).to eq(6)
       end
     end
 
-    describe '#calculate_income_for_year, #calculate_adjusted_income_for_year' do
-      it '退職年の年収は退職金を含むこと' do
-        income = build(:income, monthly_income: 1, retirement_date: 2030, retirement_pay: 1)
-        result = income.send(:calculate_income_for_year, 2030)
-        expect(result).to eq({ date: 2030, amount: 13 })
-      end
+    describe '.grouped' do
+      it '同じ年の複数の収入を合計する' do
+        allIncomeData = [
+          { date: year, amount: 5 },
+          { date: year + 5, amount: 1 },
+          { date: year + 5, amount: 2 }
+        ]
+        result = Income.send(:grouped, allIncomeData)
 
-      it '退職年以外の年収は退職金を含まないこと' do
-        income = build(:income, monthly_income: 1, retirement_date: 2030, retirement_pay: 1)
-        result = income.send(:calculate_income_for_year, 2025)
-        expect(result).to eq({ date: 2025, amount: 12 })
+        expect(result).to include(
+          { date: year, amount: 5 },
+          { date: year + 5, amount: 3 }
+        )
+      end
+    end
+
+    describe '#retirement_date=' do
+      it '退職年をDate型の年始に設定する' do
+        income = build(:income, retirement_date: "2030")
+        expect(income.retirement_date).to eq(Date.new(2030, 1, 1))
       end
     end
   end

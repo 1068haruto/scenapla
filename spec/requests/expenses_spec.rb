56697855f31b18d0e30f8b60c501cad9bfc72c1e
1068haruto <harutokasuga.610@gmail.com>
2025-11-08 15:13:14 +0900

    refactor: Add indent

--- spec/requests/expenses_spec.rb
+++ spec/requests/expenses_spec.rb
@@ -15,60 +15,60 @@ RSpec.describe "Expenses", type: :request do
   end
 
   describe "GET /index" do
-    it "ページ表示し200を返す" do
+    it "ページを表示(200)" do
       get expenses_path
-      expect(response).to have_http_status(:success)  # 200
+      expect(response).to have_http_status(:success)
     end
   end
 
   describe "POST /create" do
-    context "既に他のデータが存在する場合" do
-      it "新規作成し、古いデータを削除し、302を返す" do
-        old_expense = create(:expense, user: user, simulation: simulation)
+    context "他のデータが存在する場合" do
+      it "作成成功し、古いデータを削除(302)" do
+        oldExpense = create(:expense, user: user, simulation: simulation)
         post expenses_path, params: { expense: valid_params }
 
-        expect(user.expenses.count).to eq(1)                  # 保存済みデータが1件(新しいデータ)のみとなるを期待
-        expect(Expense.find_by(id: old_expense.id)).to be_nil # 古いデータが削除されているを期待
-        expect(response).to have_http_status(:found)          # 302
+        expect(user.expenses.count).to eq(1)  # 保存済が1件(新データ)のみを期待
+        expect(Expense.find_by(id: oldExpense.id)).to be_nil  # 古いデータが削除されるを期待
+        expect(response).to have_http_status(:found)
       end
     end
 
     context "他のデータが存在しない場合" do
-      it "新規作成し、302を返す" do
+      it "作成成功(302)" do
         post expenses_path, params: { expense: valid_params }
 
-        expect(user.expenses.count).to eq(1)                  # 保存済みデータが1件(新しいデータ)のみとなるを期待
-        expect(response).to have_http_status(:found)          # 302
+        expect(user.expenses.count).to eq(1)  # 保存済が1件(新データ)のみを期待
+        expect(response).to have_http_status(:found)
       end
     end
 
     context "無効なパラメータの場合" do
-      it "作成失敗し、422を返す" do
+      it "作成失敗(422)" do
         post expenses_path, params: { expense: invalid_params }
-        expect(response).to have_http_status(:unprocessable_entity) # 422
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
   describe "GET /edit" do
-    context "編集したいデータが存在する場合" do
-      it "編集フォームを表示し、200を返す" do
+    context "データが存在する場合" do
+      it "フォームを表示(200)" do
         get edit_expense_path(expense)
-        expect(response).to have_http_status(:success) # 200
+        expect(response).to have_http_status(:success)
       end
     end
 
-    context "編集したいデータが存在しない場合" do
-      it "編集フォームを表示せず、302を返す" do
+    context "データが存在しない場合" do
+      it "フォームを表示しない(302)" do
         get edit_expense_path(-1) # 存在しないID
-        expect(response).to have_http_status(:found) # 302
+        expect(response).to have_http_status(:found)
       end
     end
   end
 
   describe "PATCH /update" do
     context "有効なパラメータの場合" do
-      it "更新成功し、302を返す" do
+      it "更新成功(302)" do
         patch expense_path(expense), params: { expense: valid_params_for_update }
         expect(response).to have_http_status(:found)
 
@@ -78,25 +78,25 @@ RSpec.describe "Expenses", type: :request do
     end
 
     context "無効なパラメータの場合" do
-      it "更新失敗し、422を返す" do
+      it "更新失敗(422)" do
         patch expense_path(expense), params: { expense: invalid_params }
-        expect(response).to have_http_status(:unprocessable_entity) # 422
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
   describe "DELETE /destroy" do
-    context "削除したいデータが存在する場合" do
-      it "削除成功し、302を返す" do
+    context "データが存在する場合" do
+      it "削除成功(302)" do
         delete expense_path(expense)
         expect(response).to have_http_status(:found)
       end
     end
 
-    context "削除したいデータが存在しない場合" do
-      it "削除失敗し、302を返す" do
+    context "データが存在しない場合" do
+      it "削除失敗(302)" do
         delete expense_path(-1) # 存在しないID
-        expect(response).to have_http_status(:found) # 302
+        expect(response).to have_http_status(:found)
       end
     end
   end

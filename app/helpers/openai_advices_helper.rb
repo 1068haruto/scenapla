ba1eea90e152e29b5407622ce7c9775473a23146
1068haruto <harutokasuga.610@gmail.com>
2025-12-08 00:30:38 +0900

    refactor: Remove unnecessary files

--- app/helpers/openai_advices_helper.rb
+++
@@ -1,2 +0,0 @@
-module OpenaiAdvicesHelper
-end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  # トップ ページ
   describe "GET /static_pages/index" do
     it "ページを表示する（200）" do
       get root_path
       expect(response).to have_http_status(:success)
     end
   end
+
+  # 利用規約 ページ
   describe "GET /static_pages/terms" do
     it "ページを表示する（200）" do
       get terms_path
@@ -14,6 +17,7 @@ RSpec.describe "StaticPages", type: :request do
     end
   end
 
+  # プライバシーポリシー ページ
   describe "GET /static_pages/privacy" do
     it "ページを表示する（200）" do
       get privacy_path
